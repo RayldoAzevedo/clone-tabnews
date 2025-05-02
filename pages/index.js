0a3e049cd4a3b0fdf75230176079cd9a212ff47a
RayldoAzevedo <48896540+RayldoAzevedo@users.noreply.github.com>
2025-05-02 14:59:32 +0000

    editor config

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import {soma, subtracao, multiplicacao, divisao, potencia, raiz, fatorial, media} from '../src/operacoes.js'         
+import { soma, subtracao, multiplicacao, divisao, potencia, raiz, fatorial, media } from '../src/operacoes.js'
 
-function Home(){
-
-    return(
-        <div>
-            <h2>Mensagem para pessoa amada</h2>
-
-        </div>
-    )
-
+function Home() {
+  return (
+    <div>
+      <h2>Mensagem para pessoa amada</h2>
+    </div>
+  )
 }
 
-
-export default Home
+export default Home;
