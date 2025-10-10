b943c266f8d3e5dab1147c7743ae9d782bd922c2
RayldoAzevedo <48896540+RayldoAzevedo@users.noreply.github.com>
2025-10-10 14:56:44 +0000

    Testes automatizados - pasta teste

---
+++ tests/calculadora.test.js
@@ -0,0 +1,11 @@
+test("Primeiro teste", () => {
+  console.log("Executando o teste");
+});
+
+test("segundo teste", () => {
+  console.log("Executando outro teste");
+});
+
+test("esperar que 1 seja 1", () => {
+  expect(1).toBe(1);
+});
